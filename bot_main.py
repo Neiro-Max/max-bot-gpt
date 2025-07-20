c2405027455bc47beef0da0908932587a26c6d50
Neiro-Max <s.valuyko@gmail.com>
2025-07-20 19:19:59 +0300

    Update bot_main.py

--- bot_main.py
+++ bot_main.py
@@ -634,4 +634,6 @@ def yookassa_webhook():
 
 
 if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url="https://max-bot-gpt-production.up.railway.app/webhook")
     app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
